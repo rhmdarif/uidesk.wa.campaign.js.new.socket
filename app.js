2d0e9677150d36e3c9dac1b48de0b23abc850c31
Rahmad Arif <rahmadarif28080@gmail.com>
2022-02-23 22:16:52 +0700

    running

--- app.js
+++ app.js
@@ -19,13 +19,14 @@ io.on("connection", (socket) => {
     // IN - SERVER
     // OUT - SERVER
     socket.on("qr", (token, datas) => {
-        socket.emit("qr-"+token, datas);
+        console.log(token, datas);
+        io.emit("qr-"+token, datas);
     });
     socket.on("logged", (token, datas) => {
-        socket.emit("logged-"+token, datas);
+        io.emit("logged-"+token, datas);
     });
     socket.on("logout", (datas) => {
-        socket.emit("logout", datas);
+        io.emit("logout", datas);
     });
     // IN - CLIENT
 
